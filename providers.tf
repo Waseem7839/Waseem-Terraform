84a484b5a8b1d52b3ee1d451809c13626efeff62
Shaik Mohammad Waseem Akram <138425809+Waseem7839@users.noreply.github.com>
2025-08-21 10:26:17 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,19 @@
+terraform {
+     backend "s3" {
+       bucket       = "waseem-bucket-20082025"
+       key          = "waseem-terraform.tfstate"
+       region       = "eu-west-2"
+       use_lockfile = true
+     }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+
 provider "aws" {
-  region = "eu-west-2" # Change to your assigned region
+  region = "eu-west-2" 
 }
